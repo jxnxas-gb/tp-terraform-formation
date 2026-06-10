402f0e6882990aa7c8430a7f68510de515aa3cd9
jxnxas-gb <juniasgbenou0@gmail.com>
2026-06-10 11:34:47 +0200

    tp01: gestion des buckets S3 (init et secure)

---
+++ jour1/tp01-s3-secure/outputs.tf
@@ -0,0 +1,19 @@
+output "bucket_name" {
+  value       = aws_s3_bucket.main.id
+  description = "Nom unique du bucket S3 cree"
+}
+
+output "bucket_arn" {
+  value       = aws_s3_bucket.main.arn
+  description = "ARN complet du bucket S3"
+}
+
+output "bucket_region" {
+  value       = aws_s3_bucket.main.region
+  description = "Region AWS ou est deploye le bucket"
+}
+
+output "versioning_status" {
+  value       = aws_s3_bucket_versioning.main.versioning_configuration[0].status
+  description = "Statut d'activation du versioning"
+}
